4b4b1d239cdc48c70ebf013595499a925eb7f0bc
Ketch4n <mangao.christian.04@gmail.com>
2024-09-18 14:34:30 +0800

    Deployment-API

--- lib/src/data/server/url.dart
+++ lib/src/data/server/url.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class Servername with ChangeNotifier {
-  static String host = "http://127.0.0.1:8000/api/";
+  static String host = "https://ketchan-tech.online/api/";
 }
