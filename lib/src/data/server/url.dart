955d363da698c69c90d72f0efd5cff50596d0be0
Ketch4n <mangao.christian.04@gmail.com>
2024-09-20 17:09:47 +0800

    Url-Modified

--- lib/src/data/server/url.dart
+++ lib/src/data/server/url.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Servername with ChangeNotifier {
+  // PROD
   static String host = "https://ketchan-tech.online/api/";
+
+  // DEV
+  // static String host = "http://127.0.0.1:8000/api/";
 }
