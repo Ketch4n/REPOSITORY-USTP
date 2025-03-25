d3f575cf9da9623e86471a3d6c57e129b73efa00
Ketch4n <mangao.christian.04@gmail.com>
2025-03-25 21:58:25 +0800

    feat(Connectivity): show Server Error or No Internet Access

--- lib/src/auth/login/login_function.dart
+++ lib/src/auth/login/login_function.dart
@@ -5,6 +5,7 @@ import 'package:repository_ustp/src/components/snackbar.dart';
 import 'package:repository_ustp/src/data/server/url.dart';
 import 'package:repository_ustp/src/data/provider/user_session.dart';
 import 'package:http/http.dart' as http;
+import 'package:connectivity/connectivity.dart';
 
 class LoginFunctions {
   static Future fetchUserCredentials(
@@ -14,6 +15,13 @@ class LoginFunctions {
     } else {
       circularLoading(context);
       try {
+        var connectivityResult = await (Connectivity().checkConnectivity());
+        if (connectivityResult == ConnectivityResult.none) {
+          customSnackBar(context, 1, "No internet connection!");
+          Navigator.of(context).pop();
+          return;
+        }
+
         final response = await http.post(
           Uri.parse("${Servername.host}user/login"),
           body: {
@@ -28,13 +36,10 @@ class LoginFunctions {
         var message = jsonResponse['message'];
 
         if (response.statusCode == 200 && quack) {
-          // UserSession.auth = true;
           UserSession.id = user['id'];
           UserSession.username = user["username"];
           UserSession.email = user["email"];
           UserSession.type = user['type'];
-          // IndexMenuItem.quack = user['type'] == 2 ? 0 : 1;
-          // const content = "Login Success";
           customSnackBar(context, 0, message);
           Navigator.pushNamedAndRemoveUntil(
             context,
@@ -42,13 +47,12 @@ class LoginFunctions {
             (route) => false,
           );
         } else {
-          // const content = "Username or password invalid";
           customSnackBar(context, 1, message);
           Navigator.of(context).pop();
         }
       } catch (e) {
-        // final content = 'Failed to fetch users: $e';
-        customSnackBar(context, 2, e);
+        customSnackBar(context, 2, "Failed to connect to the server!");
+        Navigator.of(context).pop();
       }
     }
   }
